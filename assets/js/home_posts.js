9d0bf3fd9f8d862cc5d3a1e2c1dc0a8114793c4d
Kashish <58864414+kash1sh@users.noreply.github.com>
2020-10-04 00:01:55 +0530

    Update home_posts.js

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -72,14 +72,7 @@
     </li>`)
     }
 
-// yaha pe, hune pehle post.ejs mein likes ke liye ek a tag daala, ab hamne ajax call ke liye use home_posts.js mein daala diya, ye basically
-// call kar raha hai na, ajax call?  nahi nahi nahi ye h  ajax call -- ye sara jo `` backticks m h ye toh bss ek html structure h 
-// jo hume callback data se bna liya  or html m append kra dia bss , jaise maine posts.ejs mein already ye a tag for likes ka html daala hua
-// hai, toh yaha add karne ki kya need hai?   ok     so html m jo b lines chlri h vo tb chlri h jb page load hoga , ab yaha ajax ki wjh se 
-// page load hi ni hota -- toh agr m khali call krke chd du -- toh database m post wgerah sab bn jaega but mere page p tbtk show nhi hoga
-// jbtk refresh na kru -- or agr refesh krna pdra h toh fayda kya ajax se krne ka -- isiliye humne javascript ki help se ise append krdia 
-// kuki html elements toh hum javascript se bna skte h na 
-// acha 
+
     // Method to delete a post from DOM
     let deletePost = function(deleteLink){
         $(deleteLink).click(function(e){
