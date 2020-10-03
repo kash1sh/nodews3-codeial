89238ce65790ec7f0cbe33b7940e958656be2b1d
Kashish <58864414+kash1sh@users.noreply.github.com>
2020-10-04 00:02:39 +0530

    Update toggle_likes.js

--- assets/js/toggle_likes.js
+++ assets/js/toggle_likes.js
@@ -11,14 +11,6 @@ class ToggleLike{
             e.preventDefault();
             let self = this;
 
-            // bhai, ek last cheej bata do ki call toh humne pehle kar di thi, toh yeh us call ko accept karne ke liye hai ya kya hai?
-            // smja nahi h m kya accept krna , dekho call toh hamne abhi ajax vaali kardi thi, ab jab ham toggle likes bana rahe hai,
-            // usme jo ye hai, ye kyu hai? -- hr element  k liye uska personal hoga -- post form k liye alg hai -- comment k liye alg h
-            // create k liye alg h delete k liye alg h , esse hi likes k liye b uska alg se h (personalized for that very element)
-
-            // acha matlab vo post ke likes ko show karne ke liye call thi and ye call toggle likes ke is code ke liye, jo hamne likha hai?  bilkul
-            // badhiya bhai, samajh gaya ok :) thanks
-            // marking it resolved
 
             // this is a new way of writing ajax which you might've studied, it looks like the same as promises
             $.ajax({
